7b8817ef9a89215c315bda8d294bf25567945957
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2023-11-14 20:11:46 +0300

    add style for films cards

--- js/renders.js
+++ js/renders.js
@@ -1,4 +1,5 @@
 import { arrayToString } from "./helpers.js";
+import { eyeIcon, eyeOffIcon } from "./icons.js";
 
 
 function fragment(strings, ...values) {
@@ -26,7 +27,7 @@ export function renderFilmsList(filmsList) {
     const page = fragment/*html*/`
     <div class="films-list">
         <h1 class="page-title">Films list</h1>
-        <div class="films__list list">
+        <div class="films__list card-list">
             ${filmsList.length === 0 
                 ? /*html*/`<h5 class="no-films">No films yet.</h5>` 
                 : getFilmCardTemplate(filmsList)}
@@ -46,12 +47,12 @@ function getFilmCardTemplate(filmsList) {
             <div class="list__item card film" data-id="${film.id}">
                 <div class="card-header film-header ${film.watch ? 'film-header__watch' : ''}">
                     <h5 class="card-title">${film.title}</h5>
-                    <div class="card-icon film-status-icon">${film.watch ? 'wathcedIcon' : 'unwatchedIcon'}</div>
+                    <div class="card-icon film-status-icon">${film.watch ? eyeOffIcon() : eyeIcon()}</div>
                 </div>
                 <div class="card-content">
-                    <div class="film-description">${film.description}</div>
-                    <div class="film-genres">${arrayToString(film.genres)}</div>
-                    <div class="film-status">${film.watch ? 'whatched' : 'unwatched'}</div>
+                    <div class="film-description"><span class="card-point-name">Описание:</span> ${film.description} </div>
+                    <div class="film-genres"><span class="card-point-name">Жанры:</span> ${arrayToString(film.genres)}</div>
+                    <div class="film-status"><span class="card-point-name">Статус:</span> ${film.watch ? 'Просмотрен' : 'Не просмотрен'}</div>
                 </div>
             </div>   
         `;
